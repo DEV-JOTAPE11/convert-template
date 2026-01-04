e90cca60c3a44bc1cfa97c444d7e5b589a6e524b
joaop1109 <joaopedropiresqueiroz10@gmail.com>
2026-01-03 23:26:13 -0300

    Validando o input

--- script.js
+++ script.js
@@ -2,5 +2,6 @@ const amount = document.querySelector("#amount");
 
 // Manipulando o input amount para receber somente números.
 amount.addEventListener("input", () => {
-    
+    const hasCharactersRegex = /\D+/g
+    amount.value = amount.value.replace(hasCharactersRegex, '')
 })
