cd00d9b572ae578c25ee7c1ba6fb392574014160
joaop1109 <joaopedropiresqueiroz10@gmail.com>
2026-01-04 00:56:10 -0300

    Indentificando a moeda

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
-const form = document.querySelector("#form")
+const USD = 5.42
+const EUR = 6.43
+const GBP = 7.30
+
+
+const form = document.querySelector("form")
 const amount = document.querySelector("#amount")
 const currency = document.querySelector("#currency")
 
@@ -10,7 +15,25 @@ amount.addEventListener("input", () => {
 
 // Capturando o evento de submit do formulário
 form.onsubmit = (event) => {
-    event.preventDefault()
-
+    event.preventDefault()   
     
+    switch (currency.value) {
+        case "USD":
+            convertCurrency(amount.value, USD, "$")
+            break
+        case "EUR":
+            convertCurrency(amount.value, EUR, "€")
+            break
+        case "GBP":
+            convertCurrency(amount.value, GBP, "£")
+            break
+        default:
+            alert("Moeda não suportada.")
+    }
+}
+
+
+//Função para converter a moeda.
+function convertCurrency(amount, price, symbol) {
+   
 }
